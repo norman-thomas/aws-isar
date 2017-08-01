06867a00b89a3e4307eafc82d44dc9537947a317
Norman Thomas <news@hiroki.de>
2017-08-01 11:35:20 +0200

    implementation of info acquisition

---
+++ main.py
@@ -0,0 +1,43 @@
+import urllib.request
+from lxml.cssselect import CSSSelector
+from lxml.html import fromstring
+
+LEVEL_URL = 'http://www.hnd.bayern.de/pegel/isar/muenchen-16005701/tabelle?setdiskr=15'
+FLOW_URL = 'http://www.hnd.bayern.de/pegel/isar/muenchen-16005701/tabelle?methode=abfluss&setdiskr=15'
+TEMPERATURE_URL = 'http://www.gkd.bayern.de/fluesse/wassertemperatur/stationen/diagramm/index.php?tab=true&thema=gkd&rubrik=fluesse&produkt=wassertemperatur&msnr=783500823&gknr=4&beginn=25.07.2017&ende=01.08.2017&wertart=ezw'
+
+def lambda_handler(event, context):
+    info = fetch_info()
+    return info
+
+def fetch_info():
+    level = load_page(LEVEL_URL)
+    flow = load_page(FLOW_URL)
+    temperature = load_page(TEMPERATURE_URL)
+
+    return {
+        'level': level,
+        'flow': flow,
+        'temperature': temperature
+    }
+
+def load_page(url):
+    content = urllib.request.urlopen(url).read()
+    html = fromstring(content)
+
+    datetime_selector = CSSSelector('tbody tr:first-child td:first-child')
+    value_selector = CSSSelector('tbody tr:first-child td:last-child')
+
+    datetime = datetime_selector(html)[0].text
+    value = value_selector(html)[0].text
+
+    value = value.replace(',', '.').replace('\xa0', '')
+    value = float(value)
+
+    return {
+        'datetime': datetime,
+        'value': value
+    }
+
+if __name__ == '__main__':
+    print(fetch_info())
