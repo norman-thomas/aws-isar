eb2ae73edc9da91a14c8efb9ddefb0922bbd569a
Norman Thomas <news@hiroki.de>
2017-08-03 00:05:26 +0200

    fixed selector for temperature

--- main.py
+++ main.py
@@ -5,16 +5,29 @@ import datetime
 
 LEVEL_URL = 'http://www.hnd.bayern.de/pegel/isar/muenchen-16005701/tabelle?setdiskr=15'
 FLOW_URL = 'http://www.hnd.bayern.de/pegel/isar/muenchen-16005701/tabelle?methode=abfluss&setdiskr=15'
-TEMPERATURE_URL = 'http://www.gkd.bayern.de/fluesse/wassertemperatur/stationen/diagramm/index.php?tab=true&thema=gkd&rubrik=fluesse&produkt=wassertemperatur&msnr=783500823&gknr=4&beginn=25.07.2017&ende=01.08.2017&wertart=ezw'
+TEMPERATURE_URL = 'http://www.gkd.bayern.de/fluesse/wassertemperatur/stationen/diagramm/index.php?thema=gkd&rubrik=fluesse&produkt=wassertemperatur&gknr=4&msnr=783500823&zr=woche'
+
+LEVEL_SELECTORS = (
+    'tbody tr:nth-of-type(1) td:nth-of-type(1)',
+    'tbody tr:nth-of-type(1) td:nth-of-type(2)'
+)
+FLOW_SELECTORS = (
+    'tbody tr:nth-of-type(1) td:nth-of-type(1)',
+    'tbody tr:nth-of-type(1) td:nth-of-type(2)'
+)
+TEMPERATURE_SELECTORS = (
+    'table#pegel_tabelle tbody tr:nth-of-type(1) td:nth-of-type(1)',
+    'table#pegel_tabelle tbody tr:nth-of-type(1) td:nth-of-type(2)'
+)
 
 def lambda_handler(event, context):
     info = fetch_info()
     return info
 
 def fetch_info():
-    level = load_page(LEVEL_URL)
-    flow = load_page(FLOW_URL)
-    temperature = load_page(TEMPERATURE_URL)
+    level = load_page(LEVEL_URL, LEVEL_SELECTORS)
+    flow = load_page(FLOW_URL, FLOW_SELECTORS)
+    temperature = load_page(TEMPERATURE_URL, TEMPERATURE_SELECTORS)
 
     return {
         'level': level,
@@ -22,12 +35,12 @@ def fetch_info():
         'temperature': temperature
     }
 
-def load_page(url):
+def load_page(url, selectors):
     content = urllib.request.urlopen(url).read()
     soup = Soup(content)
 
-    datetime_str = soup.select('tbody tr:nth-of-type(1) td:nth-of-type(1)')[0].text
-    value = soup.select('tbody tr:nth-of-type(1) td:nth-of-type(2)')[0].text
+    datetime_str = soup.select(selectors[0])[0].text
+    value = soup.select(selectors[1])[0].text
 
     dt = datetime.datetime.strptime(datetime_str, '%d.%m.%Y %H:%M')
     datetime_str = dt.isoformat()
